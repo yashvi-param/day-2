0c427d65087c38a1eb937916ff9a7dbdeda5a50e
yashvi-param <yashvirwn123@gmail.com>
2025-03-08 10:50:53 +0530

    Add files via upload
    complete day-2

---
+++ index2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+  
+    int a; // declaration
+    a = 100; // assigning
+ 
+    a = 200; // re assigning;
+
+    int b = 200;
+
+    cout << "Sum of a & b = ";
+        cout << a+b;
+
+    return 0;
+}
